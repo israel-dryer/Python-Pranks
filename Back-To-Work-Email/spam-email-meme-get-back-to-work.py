8888d833a0afe11432975229ea986d617ae372e2
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-06-24 21:57:56 -0400

    update files

--- Back-To-Work-Email/spam-email-meme-get-back-to-work.py
+++ Back-To-Work-Email/spam-email-meme-get-back-to-work.py
@@ -3,8 +3,8 @@ PYTHON PRANK - Spam your co-workers with repeated meme emails
 By Israel Dryer | June 24, 2019
 
 OVERVIEW
-This is a FUN prank to pull on your friends... if they have a sense of humor! This script will send 
-a random meme to your list of contacts every 60 seconds for as many iterations as you select.
+This is a fun prank** to pull on your friends, if they have a sense of humor! This script will send 
+a random meme to a list of recipients every 60 seconds for as many times as you indicate.
 
 Use the images that I've selected, or add your own. All you need to do is run this script in the 
 same location as another folder called 'img' that contains the images that you would like to attach 
@@ -18,68 +18,83 @@ that feature. However, you should do this at your own peril, and after carefully
 warnings from the link below: https://support.google.com/accounts/answer/6010255
 
 REFERENCES
-- Email examples and libraries: https://docs.python.org/3.7/library/email.examples.html
-- time.sleep(): https://docs.python.org/3.7/library/time.html#time.sleep
-- random.randint(): https://docs.python.org/3/library/random.html#random.randint
-- os.listdir(): https://docs.python.org/3/library/os.html#os.listdir
+- smtplib: https://docs.python.org/3/library/smtplib.html#module-smtplib
+- email.meme.multipart: https://docs.python.org/3.7/library/email.mime.html#email.mime.multipart.MIMEMultipart
+- email.meme.text: https://docs.python.org/3.7/library/email.mime.html#email.mime.text.MIMEText
+- email.meme.image: https://docs.python.org/3.7/library/email.mime.html#email.mime.image.MIMEImage
+- time.sleep: https://docs.python.org/3.7/library/time.html#time.sleep
+- random.randint: https://docs.python.org/3/library/random.html#random.randint
+- os.listdir: https://docs.python.org/3/library/os.html#os.listdir
+- email examples: https://docs.python.org/3.7/library/email.examples.html
 
 """
-### import project libraries
-import os
-import smtplib
-import random
-import time
-from email.message import EmailMessage
+# import project libraries
+import os # used to locate the image files
+import random # used to generate a random number for pulling an image from the list
+import time # used to create a delay between sending emails
+import smtplib as smtp # used for actually sending the email
+
+## used for building the email message
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-### setup server authentication
-username = 'user.name@gmail.com'
+
+# setup server authentication variables
+user = 'user@gmail.com'
 password = 'pas$w0rd'
 fr_address = 'me@gmail.com'
-to_address = 'you1@gmail.com, you2@gmail.com, you3@gmail.com'
+to_address = 'you1@gmail.com, you2@gmail.com, you3@gmail.com, you4@gmail.com'
 smtp_host = 'smtp.gmail.com'
 
-### setup message
-subject = 'Get BACK to WORK!!'
-message = 'What do you think you\'re doing?'
-num_of_msg = 10 
+# setup email variables
+subject = 'Get back to WORK!!'
+msg_text = 'what do you think you\'re doing?'
+msg_count = 20
 
-### create and send the message
-# get a list of all images in the img directory
+
+# create email message
+
+## get a list of all messages in the image directory
 memes = os.listdir('img')
 
-# reference the image in the IMG SRC attribute by the ID we give it below
-msgText = MIMEText(f'<b><i>{message}</i></b><br><br><img src="cid:image1">', 'html')
+## create the multipart email message and add from ,to, and subject headers
+msg = MIMEMultipart()
+msg['From'] = fr_address
+msg['To'] = to_address
+msg['Subject'] = subject
 
-for i in range(num_of_msg):
+## create an html message with a reference to the image source attribute referenced below
+body = MIMEText(f'<b><i>{msg_text}</b></i><br><br><img src="cid:myimage"/>','html')
+
+## open a connection to the email server
+server = smtp.SMTP(host=smtp_host, port=587)
+server.starttls()
+server.login(user=user, password=password)
+
+## create and send the message
+for i in range(msg_count):
     
-    # random number generator for meme selection
-    rand_num = random.randint(0,len(memes)-1)
-    attachment = 'img/' + memes[rand_num]
+    # open a random image from the image directory
+    rnd_image = memes[random.randint(0,len(memes)-1)]
+    with open(f'img/{rnd_image}','rb') as f:
+        msg_image = MIMEImage(f.read())
 
-    # create the message and fill in the from, to, and subject headers
-    msg = MIMEMultipart('related')
-    msg['From'] = fr_address
-    msg['To'] = to_address
-    msg['Subject'] = subject
-    msg.attach(msgText)
+    # create the message content ID referenced above
+    msg_image.add_header('Content-ID','myimage')
 
-    # open the attachment chosen above
-    with open(attachment, 'rb') as img:
-        msgImage = MIMEImage(img.read())
+    # set the message payload
+    '''
+    We can't use the MIMEText.attach() method here otherwise it will add additional parts
+    each consecutive email. Using the MIMEText.set_payload() method basically resets this 
+    each time
+    '''
+    msg.set_payload([body, msg_image])
 
-    # define the image's ID as referenced above
-    msgImage.add_header('Content-ID', '<image1>')
-    msg.attach(msgImage)
-
-    # send the email
-    server = smtplib.SMTP(host=smtp_host, port=587)
-    server.starttls()
-    server.login(username, password)
+    # open the email server and send the message
     server.send_message(msg)
-    server.close()
     
-    # wait 60 seconds before looping
+    # delay the next message by 60 seconds... you can change this if you want
     time.sleep(60)
+
+server.close()
